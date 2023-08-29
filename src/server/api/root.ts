68f701f8d7f7a41a503594048c8b6da89371a4a1
nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-08-29 14:07:31 +0200

    started game router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { userRouter } from "./routers/user";
+import { gameRouter } from "./routers/game";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { userRouter } from "./routers/user";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   user: userRouter,
+  game: gameRouter,
 });
 
 // export type definition of API
